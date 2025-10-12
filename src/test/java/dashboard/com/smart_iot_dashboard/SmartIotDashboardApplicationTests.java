529cb72ef1b69dbcd7963f25953b1e5eed1e7d5f
Valerii <burdulenkovalera@gmail.com>
2025-10-12 21:22:42 +0200

    Spring initialise

---
+++ src/test/java/dashboard/com/smart_iot_dashboard/SmartIotDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package dashboard.com.smart_iot_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartIotDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
